d7485af2986afc8f4745d20f61e82b1b1696f526
MaQueen <67810971+kotobuki562@users.noreply.github.com>
2021-08-01 20:05:55 +0900

    users

--- src/libs/auth0.ts
+++ src/libs/auth0.ts
@@ -10,24 +10,30 @@ export const useSwrUsers = (
     const jwt = await axios.post(
       process.env.AUTH0_MANAGEMENT_CLIENT_URI as string,
       {
-        client_id: process.env.AUTH0_MANAGEMENT_CLIENT_AUDIENCE as string,
-        client_secret: process.env.REACT_APP_AUTH0_USER_CLIENT_SECRET as string,
+        client_id: process.env.AUTH0_MANAGEMENT_CLIENT_ID as string,
+        client_secret: process.env.AUTH0_MANAGEMENT_CLIENT_SECRET as string,
         audience: `${process.env.AUTH0_MANAGEMENT_CLIENT_AUDIENCE}/`,
         grant_type: 'client_credentials',
       },
-      { headers: { 'content-type': 'application/json' } }
+      {
+        headers: {
+          'content-type': 'application/json',
+          'Access-Control-Allow-Origin': 'http://localhost:3000',
+        },
+      }
     );
+
     const res = await axios.get(url, {
       headers: {
         'Content-Type': 'application/json',
         Accept: 'application/json',
-        authorization: `Bearer ${jwt.data.access_token}`,
+        authorization: `Bearer ${jwt.data.access_token as string}`,
       },
     });
     return res.data;
   }, []);
   const { data, error } = useSWR(
-    `${process.env.REACT_APP_AUTH0_USER_AUDIENCE + url}`,
+    `${process.env.AUTH0_MANAGEMENT_CLIENT_AUDIENCE + url}`,
     swrFetcher
   );
 
